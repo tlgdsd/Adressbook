3dc2433128b68160c81b17cfd235d5324ddd702b
tlgdsd <53423376+tlgdsd@users.noreply.github.com>
2020-10-08 23:17:50 +0300

    Debug
    view kısmındaki bazı dil hataları giderildi.

--- Adressbook/Models/Contact.cs
+++ Adressbook/Models/Contact.cs
@@ -12,7 +12,9 @@ namespace Adressbook.Models
         public int Id { get; set; }
         [Required]
         public ApplicationUser User { get; set; }
+        [Display(Name = "Adı")]
         public string Name { get; set; }
+        [Display(Name = "Soyadı")]
         public string Surname { get; set; }
         public bool IsDeleted { get; set; }
         public List<Address> Addresses { get;set; }
