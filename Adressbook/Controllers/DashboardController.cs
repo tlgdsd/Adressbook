9b6e71b7c7cd87b544b10ed06d4ccc5a2b275629
tlgdsd <53423376+tlgdsd@users.noreply.github.com>
2020-10-08 22:39:53 +0300

    optimization
    optimized error handling, less calls to database, added try-catch to database calls

--- Adressbook/Controllers/DashboardController.cs
+++ Adressbook/Controllers/DashboardController.cs
@@ -29,197 +29,321 @@ namespace Adressbook.Controllers
 
         public ActionResult Index()
         {
-            string userid = User.Identity.GetUserId();
-            List<Contact> contactList = (from i in _context.Contacts where i.User.Id == userid && !i.IsDeleted select i).ToList();
-            foreach (Contact contact in contactList)
+            try
             {
-                contact.Addresses = (from i in _context.Adresses where i.Contact.Id == contact.Id && !i.IsDeleted select i).ToList();
+                string userid = User.Identity.GetUserId();
+                List<Contact> contactList = (from i in _context.Contacts where i.User.Id == userid && !i.IsDeleted select i).ToList();
+                foreach (Contact contact in contactList)
+                {
+                    contact.Addresses = (from i in _context.Adresses where i.Contact.Id == contact.Id && !i.IsDeleted select i).ToList();
+                }
+                return View(contactList);
             }
-            return View(contactList);
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
         }
-        //CONTACTS
-        private bool ContactBelongsToUser(int id)
+
+        #region Contact
+
+        private bool CheckContact(Contact contact)
         {
-            string userid = User.Identity.GetUserId();
-            bool check = _context.Contacts.Where(m => m.User.Id == userid && m.Id == id).Count() > 0;
-            return (check);
+            if (contact == null)
+                return false;
+            return contact.User.Id == User.Identity.GetUserId() && !contact.IsDeleted;
         }
-        public ActionResult NewContactForm ()
+        public ActionResult NewContactForm()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddContact (ContactViewModel viewModel)
+        public ActionResult AddContact(ContactViewModel viewModel)
         {
-            Contact newcontact = new Contact
+            try
             {
-                Name = viewModel.Name,
-                Surname = viewModel.Surname,
-                IsDeleted = false,
-            };
-            newcontact.User = _context.Users.Find(User.Identity.GetUserId());
-            Address newaddress = new Address
-            {
-                AddressText = viewModel.AddressText,
-                City = viewModel.AddressCity,
-                ZipCode = viewModel.AddressZipCode,
-                Phone = viewModel.AddressPhone,
-                Contact = newcontact,
-                IsDeleted = false,
-            };
-            _context.Contacts.Add(newcontact);
-            _context.Adresses.Add(newaddress);
-            _context.SaveChanges();
-
-            return RedirectToAction("Index");
-        }
-        public ActionResult ContactDetails (int id)
-        {
-            if (ContactBelongsToUser(id))
-            {
-                Contact contact = _context.Contacts.Find(id);
-                contact.Addresses = (from i in _context.Adresses where i.Contact.Id == contact.Id && !i.IsDeleted select i).ToList();
-
-                return View(contact);
-            }
-            return View("Error");
-        }
-
-        public ActionResult EditContactForm (int id)
-        {
-            if (ContactBelongsToUser(id))
-            {
-                Contact contact = _context.Contacts.Find(id);
-                ContactViewModel contactViewModel = new ContactViewModel
+                Contact newcontact = new Contact
                 {
-                    Id = id,
-                    Name = contact.Name,
-                    Surname = contact.Surname
-
+                    Name = viewModel.Name,
+                    Surname = viewModel.Surname,
+                    IsDeleted = false,
                 };
-                return View(contactViewModel);
-            }
-            return View("Error");
-        }
-        [HttpPost]
-        public ActionResult EditContact (ContactViewModel viewModel)
-        {
-            Contact contact = _context.Contacts.Include(m => m.User).SingleOrDefault(m => m.Id == viewModel.Id);
-            contact.Name = viewModel.Name;
-            contact.Surname = viewModel.Surname;
-            _context.Contacts.AddOrUpdate(contact);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        public ActionResult DeleteContact (int id)
-        {
-            if (ContactBelongsToUser(id))
-            {
-                Contact contact = _context.Contacts.Find(id);
-                contact.IsDeleted = true;
-                List<Address> contactaddresses = _context.Adresses.Where(m => m.Contact.Id == contact.Id).ToList();
-                foreach (Address address in contactaddresses)
+                newcontact.User = _context.Users.Find(User.Identity.GetUserId());
+                Address newaddress = new Address
                 {
-                    address.IsDeleted = true;
-                    _context.Adresses.AddOrUpdate(address);
-                }
-                _context.Contacts.AddOrUpdate(contact);
+                    AddressText = viewModel.AddressText,
+                    City = viewModel.AddressCity,
+                    ZipCode = viewModel.AddressZipCode,
+                    Phone = viewModel.AddressPhone,
+                    Contact = newcontact,
+                    IsDeleted = false,
+                };
+                _context.Contacts.Add(newcontact);
+                _context.Adresses.Add(newaddress);
                 _context.SaveChanges();
+
                 return RedirectToAction("Index");
             }
-            return View("Error");
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+        }
+        public ActionResult ContactDetails(int id)
+        {
+            try
+            {
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(d => d.Id == id);
+                if (CheckContact(contact))
+                {
+                    contact.Addresses = (from i in _context.Adresses where i.Contact.Id == contact.Id && !i.IsDeleted select i).ToList();
+
+                    return View(contact);
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
         }
 
-        //ADDRESSES
-        private bool AddressBelongsToUser(int id)
+        public ActionResult EditContactForm(int id)
         {
-            string userid = User.Identity.GetUserId();
-            bool check = _context.Adresses.Where(m => m.Id == id && m.Contact.User.Id == userid).Count() > 0;
-            return check;
+            try
+            {
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == id);
+                if (CheckContact(contact))
+                {
+                    ContactViewModel contactViewModel = new ContactViewModel
+                    {
+                        Id = id,
+                        Name = contact.Name,
+                        Surname = contact.Surname
+
+                    };
+                    return View(contactViewModel);
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult EditContact(ContactViewModel viewModel)
+        {
+            try
+            {
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == viewModel.Id);
+                if (CheckContact(contact))
+                {
+                    contact.Name = viewModel.Name;
+                    contact.Surname = viewModel.Surname;
+                    _context.Contacts.AddOrUpdate(contact);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+
+        }
+        public ActionResult DeleteContact(int id)
+        {
+            try
+            {
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == id);
+                if (CheckContact(contact))
+                {
+                    contact.IsDeleted = true;
+                    List<Address> contactaddresses = _context.Adresses.Where(m => m.Contact.Id == contact.Id).ToList();
+                    foreach (Address address in contactaddresses)
+                    {
+                        address.IsDeleted = true;
+                        _context.Adresses.AddOrUpdate(address);
+                    }
+                    _context.Contacts.AddOrUpdate(contact);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+        }
+
+        #endregion
+
+        #region Address
+
+        private bool CheckAddress(Address address)
+        {
+            if (address == null)
+                return false;
+
+            return address.Contact.User.Id == User.Identity.GetUserId() && !address.IsDeleted;
         }
 
         public ActionResult NewAddressForm(int id)
         {
-            if (ContactBelongsToUser(id))
+            try
             {
-                AddressViewModel addressViewModel = new AddressViewModel
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == id);
+                if (CheckContact(contact))
                 {
-                    ContactId = id
-                };
-                return View(addressViewModel);
+                    AddressViewModel addressViewModel = new AddressViewModel
+                    {
+                        ContactId = id
+                    };
+                    return View(addressViewModel);
+                }
+                return View("NotFound");
             }
-            return RedirectToAction("Index");
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
         }
 
         [HttpPost]
-        public ActionResult AddAddress (AddressViewModel viewModel)
+        public ActionResult AddAddress(AddressViewModel viewModel)
         {
-            Address address = new Address
+            try
             {
-                AddressText = viewModel.AddressText,
-                City = viewModel.City,
-                ZipCode = viewModel.ZipCode,
-                Phone = viewModel.Phone,
-                IsDeleted = false,
-            };
-            address.Contact = _context.Contacts.Find(viewModel.ContactId);
-            _context.Adresses.Add(address);
-            _context.SaveChanges();
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == viewModel.ContactId);
+                if (CheckContact(contact))
+                {
+                    Address address = new Address
+                    {
+                        AddressText = viewModel.AddressText,
+                        City = viewModel.City,
+                        ZipCode = viewModel.ZipCode,
+                        Phone = viewModel.Phone,
+                        IsDeleted = false,
+                    };
+                    address.Contact = _context.Contacts.Find(viewModel.ContactId);
+                    _context.Adresses.Add(address);
+                    _context.SaveChanges();
 
-            return RedirectToAction("ContactDetails", new { id = viewModel.ContactId});
+                    return RedirectToAction("ContactDetails", new { id = viewModel.ContactId });
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+
         }
 
-        public ActionResult EditAddressForm (int id)
+        public ActionResult EditAddressForm(int id)
         {
-            if (AddressBelongsToUser(id))
+            try
             {
-                Address address = _context.Adresses.Include(m => m.Contact).SingleOrDefault(m => m.Id == id);
-                AddressViewModel addressViewModel = new AddressViewModel
+                Address address = _context.Adresses.Include("Contact").Include("Contact.User").FirstOrDefault(m => m.Id == id);
+                if (CheckAddress(address))
                 {
-                    Id = id,
-                    ContactId = address.Contact.Id,
-                    AddressText = address.AddressText,
-                    City = address.City,
-                    ZipCode = address.ZipCode,
-                    Phone = address.Phone
-                };
-                return View(addressViewModel);
+                    AddressViewModel addressViewModel = new AddressViewModel
+                    {
+                        Id = id,
+                        ContactId = address.Contact.Id,
+                        AddressText = address.AddressText,
+                        City = address.City,
+                        ZipCode = address.ZipCode,
+                        Phone = address.Phone
+                    };
+                    return View(addressViewModel);
+                }
+                return View("NotFound");
             }
-            return View("Error");
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
         }
         [HttpPost]
-        public ActionResult EditAddress (AddressViewModel viewModel)
+        public ActionResult EditAddress(AddressViewModel viewModel)
         {
-            Contact contact = _context.Contacts.Find(viewModel.ContactId);
-            Address address = new Address
+            try
             {
-                Id = viewModel.Id,
-                Contact = contact,
-                IsDeleted = false,
-                AddressText = viewModel.AddressText,
-                City = viewModel.City,
-                ZipCode = viewModel.ZipCode,
-                Phone = viewModel.Phone
-            };
-            _context.Adresses.AddOrUpdate(address);
-            _context.SaveChanges();
+                Contact contact = _context.Contacts.Include("User").FirstOrDefault(m => m.Id == viewModel.ContactId); ;
+                if (CheckContact(contact))
+                {
+                    Address address = new Address
+                    {
+                        Id = viewModel.Id,
+                        Contact = contact,
+                        IsDeleted = false,
+                        AddressText = viewModel.AddressText,
+                        City = viewModel.City,
+                        ZipCode = viewModel.ZipCode,
+                        Phone = viewModel.Phone
+                    };
+                    _context.Adresses.AddOrUpdate(address);
+                    _context.SaveChanges();
 
-            return RedirectToAction("ContactDetails", new { id = viewModel.ContactId });
-        }
-        public ActionResult DeleteAddress (int id)
-        {
-            if (AddressBelongsToUser(id))
-            {
-                Address address = _context.Adresses.Include(m => m.Contact).Where(m => m.Id == id).First();
-                address.IsDeleted = true;
-                _context.Adresses.AddOrUpdate(address);
-                _context.SaveChanges();
-                return RedirectToAction("ContactDetails", new { id = address.Contact.Id });
+                    return RedirectToAction("ContactDetails", new { id = viewModel.ContactId });
+                }
+                return View("NotFound");
             }
-            return View("Error");
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
+
+        }
+        public ActionResult DeleteAddress(int id)
+        {
+            try
+            {
+                Address address = _context.Adresses.Include("Contact").Include("Contact.User").FirstOrDefault(m => m.Id == id);
+                if (CheckAddress(address))
+                {
+                    address.IsDeleted = true;
+                    _context.Adresses.AddOrUpdate(address);
+                    _context.SaveChanges();
+                    return RedirectToAction("ContactDetails", new { id = address.Contact.Id });
+                }
+                return View("NotFound");
+            }
+            catch (Exception)
+            {
+                if (_context != null)
+                    _context.Dispose();
+                return View("Error");
+            }
         }
 
-
+        #endregion
     }
 }
